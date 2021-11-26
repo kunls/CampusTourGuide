2e9cbca7e1d77a00f4592ae57c71b9659bd8f660
lxk <1921205274@qq.com>
2021-11-26 21:46:42 +0800

    My third commit

--- src/main/java/com/lxk/git/gitTest.java
+++ src/main/java/com/lxk/git/gitTest.java
@@ -1,12 +1,10 @@
 package com.lxk.git;
 
-/**
- * @author lxkstart
- * @date 2021/11/26 - 21:22
- */
+
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("hello,git2!");
+        System.out.println("hello,git3!");
     }
 }
