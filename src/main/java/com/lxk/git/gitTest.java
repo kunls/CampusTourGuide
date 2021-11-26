9ffda580e030d6d2b64cdae1a7983f1ba324e7ae
lxk <1921205274@qq.com>
2021-11-26 21:42:44 +0800

    My first commit

---
+++ src/main/java/com/lxk/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.lxk.git;
+
+/**
+ * @author lxkstart
+ * @date 2021/11/26 - 21:22
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
